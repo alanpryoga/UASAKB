44c86d6bcb67423ab32508c6ddbf983390da5931
Ade Syahlan Prayoga <imalanpryoga@gmail.com>
2020-08-07 17:03:02 +0700

    Membuat database handler

---
+++ app/src/main/java/com/alanpryoga/uasakb/data/DbHandler.java
@@ -0,0 +1,33 @@
+/**
+ * NIM      : 10117210
+ * Nama     : Ade Syahlan Prayoga
+ * Kelas    : IF-7
+ */
+package com.alanpryoga.uasakb.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_NAME = "db_uas_akb";
+
+    public DbHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(DbTable.CREATE_LOKASI_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(DbTable.DROP_LOKASI_TABLE);
+
+        onCreate(sqLiteDatabase);
+    }
+}
---
+++ app/src/main/java/com/alanpryoga/uasakb/data/DbTable.java
@@ -0,0 +1,28 @@
+/**
+ * NIM      : 10117210
+ * Nama     : Ade Syahlan Prayoga
+ * Kelas    : IF-7
+ */
+package com.alanpryoga.uasakb.data;
+
+public class DbTable {
+
+    public static final String LOKASI_TABLE             = "tb_lokasi";
+
+    public static final String LOKASI_FIELD_ID          = "id";
+    public static final String LOKASI_FIELD_NAMA        = "nama";
+    public static final String LOKASI_FIELD_DESKRIPSI   = "deskripsi";
+    public static final String LOKASI_FIELD_GAMBAR      = "gambar";
+    public static final String LOKASI_FIELD_LONGITUDE   = "longitude";
+    public static final String LOKASI_FIELD_LATITUDE    = "latitude";
+
+    public static final String CREATE_LOKASI_TABLE = "CREATE TABLE " + LOKASI_TABLE + " (" +
+            LOKASI_FIELD_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            LOKASI_FIELD_NAMA + " CHAR(70) NOT NULL, " +
+            LOKASI_FIELD_DESKRIPSI + " TEXT, " +
+            LOKASI_FIELD_GAMBAR + " BLOB, " +
+            LOKASI_FIELD_LONGITUDE + " CHAR(70), " +
+            LOKASI_FIELD_LATITUDE + " CHAR(70)" + " )";
+
+    public static final String DROP_LOKASI_TABLE = "DROP TABLE " + LOKASI_TABLE;
+}
